1c005c9087440441155aa9826c59fed805a004d8
Ekansh Lamba <ekanshlamba6226@gmail.com>
2026-09-12 23:00:32 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 98.63%] - LeetPush

---
+++ 75-Sort-Colors.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        for i in range(0,n):
+            min_ind=i
+            for j in range(i+1,n):
+                if (nums[j]<nums[min_ind]):
+                    min_ind=j
+            nums[i],nums[min_ind]=nums[min_ind],nums[i]
