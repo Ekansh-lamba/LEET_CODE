911a43865306624f539a5387b1cfcafa6e0f1841
Ekansh Lamba <ekanshlamba6226@gmail.com>
2026-09-16 01:10:05 +0530

    [485] [Time Beats: 66.65%] [Memory Beats: 43.88%] - LeetPush

---
+++ 485-Max-Consecutive-Ones.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        cnt=0
+        mxcnt=0
+        for i in range(len(nums)):
+            if(nums[i]==1):
+                cnt+=1
+                mxcnt=max(cnt,mxcnt)
+            else:
+                
+                cnt=0
+        return mxcnt
